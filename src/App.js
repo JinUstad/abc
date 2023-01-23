ad112e532a684c6b72c99bbf95306d387bdcc16d
Jin Ustad <jin.gul0628@gmail.com>
2023-01-23 18:43:18 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import EdiMain from './Component/EdiMain';
+// import Main from './Google/Main';
+
+function App() {
+  return (
+    <>
+      {/* <Main/>  */}
+      <EdiMain/>
+    </>
+  );
+}
+
+export default App;
